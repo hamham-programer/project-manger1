00a24e3b6365623c639e797cae81dbc3e419afb3
hamidreza <cheeta.code2022@gmail.com>
2023-08-21 08:54:18 +0330

    Add development mode and real mode

--- environment/app.js
+++ environment/app.js
@@ -1,14 +1,22 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
+const path = require("path")
 dotenv.config()
+const NodeEnv = process.env.NODE_ENV
+dotenv.config({
+   path:path.join(__dirname,`.env.${NodeEnv}`)
+
+})
 app.get("/", (req,res,next) =>{
     console.log(process.env.APIKEY)
+    console.log(process.env.MESSAGE)
+
     res.send("dotenv")
+
 })
 
 
 app.listen(process.env.PORT, ()=>{
-    console.log("server run on port"+ process.env.PORT);
+    console.log("server run on port"+ " "+ process.env.PORT)
 })
-
