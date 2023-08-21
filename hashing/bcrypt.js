3867d221b84491316e3aa17603cc7f0f084fa0b6
hamidreza <cheeta.code2022@gmail.com>
2023-08-21 12:20:53 +0330

    hashing in bcrypt

--- hashing/bcrypt.js
+++ hashing/bcrypt.js
@@ -1,3 +1,16 @@
 const bcrypt = require("bcrypt")
-const hash = bcrypt.genSaltSync(10)
-console.log(hash, hash.length);
+
+function hashpassword(password) {
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    return hash
+  //  console.log(hash, hash.length);
+}
+
+function verifypassword(password,hashPassword) {
+     return bcrypt.compareSync(password,hashPassword)
+    
+}
+const hash = hashpassword("123456")
+const result = verifypassword("1234567",hash )
+console.log(result);
